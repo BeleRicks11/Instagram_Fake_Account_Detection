040d60232d2d5f54323841e7cba20738c6bbffd9
danielepelleg <55688016+danielepelleg@users.noreply.github.com>
2020-03-12 00:53:32 +0100

    Add another 300 realUsers to dataset and delete duplicates

--- UsernameScraper.py
+++ UsernameScraper.py
@@ -7,11 +7,11 @@ instagram.login(force=False,two_step_verificator=True)
 
 sleep(20) # Delay to mimic user
 
-username = 'am.e.lie'
+username = 'pewdiepie'
 followings = []
 account = instagram.get_account(username)
 sleep(1)
-followings = instagram.get_following(account.identifier, 150, 50, delayed=True) # Get 150 followers of 'username', 100 a time with random delay between requests
+followings = instagram.get_following(account.identifier, 100, 50, delayed=True) # Get 150 followers of 'username', 100 a time with random delay between requests
 
 for following in followings['accounts']:
     if following.is_private==False:
