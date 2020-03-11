7e89e1c1d1b40e288eaeb6d530a0303246f1d009
BeleRicks11 <riccardo.fava1@studenti.unipr.it>
2020-03-11 21:38:39 +0100

    Update real_users.txt

--- UsernameScraper.py
+++ UsernameScraper.py
@@ -2,16 +2,17 @@ from igramscraper.instagram import Instagram # pylint: disable=no-name-in-module
 from time import sleep
 
 instagram = Instagram()
-instagram.with_credentials('username', 'password')
+instagram.with_credentials('pietro.sonnini00', 'F&#yig4x0j5D')
 instagram.login(force=False,two_step_verificator=True)
 
 sleep(20) # Delay to mimic user
 
-username = 'neymarjr'
-followers = []
+username = 'am.e.lie'
+followings = []
 account = instagram.get_account(username)
 sleep(1)
-followers = instagram.get_followers(account.identifier, 150, 100, delayed=True) # Get 150 followers of 'kevin', 100 a time with random delay between requests
+followings = instagram.get_following(account.identifier, 150, 50, delayed=True) # Get 150 followers of 'username', 100 a time with random delay between requests
 
-for follower in followers['accounts']:
-    print(follower.username)
+for following in followings['accounts']:
+    if following.is_private==False:
+        print(following.username)
