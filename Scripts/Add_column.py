682ffba1171380a811476cc4f8fd4486c0074327
BeleRicks11 <riccardo.fava1@studenti.unipr.it>
2020-03-21 18:50:58 +0100

    Update Dataset.csv and add test of other ML algorithms

---
+++ Scripts/Add_column.py
@@ -0,0 +1,22 @@
+import csv
+from csv import writer
+from csv import reader
+ 
+counter=0
+# Open the input_file in read mode and output_file in write mode
+with open('DataSet2.csv', 'r') as read_obj: 
+    with open('DataSet.csv', 'w', newline='') as write_obj:
+    # Create a csv.reader object from the input file object
+        csv_reader = reader(read_obj)
+        # Create a csv.writer object from the output file object
+        csv_writer = writer(write_obj)
+        # Read each row of the input csv file as list
+        for row in csv_reader:
+            # Append the default text in the row / list
+            if(counter<=600):
+                row.append('0')
+            else: 
+                row.append('1')
+            counter=counter+1
+            # Add the updated row / list to the output file
+            csv_writer.writerow(row)
---
+++ Scripts/Remove_column.py
@@ -0,0 +1,19 @@
+import csv
+
+input_file = 'DataSet1.csv'
+output_file = 'DataSet.csv'
+cols_to_remove = [11] # Column indexes to be removed (starts at 0)
+
+cols_to_remove = sorted(cols_to_remove, reverse=True) # Reverse so we remove from the end first
+row_count = 0 # Current amount of rows processed
+
+with open(input_file, "r") as source:
+    reader = csv.reader(source)
+    with open(output_file, "w", newline='') as result:
+        writer = csv.writer(result)
+        for row in reader:
+            row_count += 1
+            print('\r{0}'.format(row_count), end='') # Print rows processed
+            for col_index in cols_to_remove:
+                del row[col_index]
+            writer.writerow(row)
